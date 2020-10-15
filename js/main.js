f786429ef7c827ae6f1c5b1b2806f8bf631b5286
Alberto <alberto.abate87@gmail.com>
2020-10-15 15:31:59 +0200

    parte mail con commenti

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // esercizio mail
 var accounts = ["antonio@gmail.com", "marcello@hotmail.com", "luigi@gmail.com"];
-
+// chiediamo la mail
 var mail = prompt("quale è la tua mail?").toLowerCase();
-
+// inserimenrto mail sbagliato falsa
 var mailFound = false;
 
 for (var i = 0; i < accounts.length; i++) {
 
   var item = accounts[i]
-
+//  se inserimento mail giusto è vero
   if (mail == item) {
       mailFound = true;
   }
 }
-
+// se mail corretta puoi accedere altrimenti no
 if (mailFound == true) {
   console.log('mail corretta, puoi accedere' );
 } else {
